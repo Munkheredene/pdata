815aff99828ce2881b71e5673e958f1bc8231133
Munkheredene <165350117+Munkheredene@users.noreply.github.com>
2024-03-29 15:26:54 +0800

    test_data

---
+++ untitled.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+count, bins, ignored = plt.hist(s, 30, density=True)
+
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+
+               np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+
+         linewidth=2, color='r')
+
+plt.show()
